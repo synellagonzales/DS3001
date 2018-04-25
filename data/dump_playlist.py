0fa6bd353cd199a455d004134b014cb38d0ec6a1
Kevin O'Brien <kjrobrien@gmail.com>
2018-04-24 22:22:13 -0400

    Revert dump_playlist.py

--- data/dump_playlist.py
+++ data/dump_playlist.py
@@ -9,34 +9,64 @@ client_credentials_manager = SpotifyClientCredentials(client_id=config.client_id
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
-tracks = \
-['4G3O4PWSHPAdNSZkkmFwDs', '0YZkqQBk2gWAFCJs59XqPg', '3KTtvScjwPFoKzUW7frj8X',
- '2Mor7Tp1w61mPKozLewqey', '3FhBQdBnnsxka6vGVcgCpX', '3vtSoQokbluZbNfVpZy162',
- '4DODnpnNZMZD1jR6NvyPuc', '6pBlce4wuq7xQehbnPhXTx', '73pVOHkzEguC4B2tc8s00e',
- '72JGbnSubT6KxXmEtXVqic', '1YA3pVpphCohL6V53qLgQ0', '2Ifjrl6lpeH0vfWiA3MUhv',
- '0sTSwUb3f6ohMHlLkIAN55', '1bLt25zgVGg9O39oT6lHw7', '3hUN9ojcHaKSQk0MgMFWkf',
- '29lZ9v5hZ7iqMMsD9nf714', '02CPGci789jBYhpFbEiu7P', '08zJpaUQVi9FrKv2e32Bah',
- '1vNoA9F5ASnlBISFekDmg3', '5t3rLuQ9yj1iScUyhgX2pk']
-
-trackfeatures = {}
-
-trackresults = sp.tracks(tracks)
-
-for track in trackresults['tracks']:
-    trackid = 'spotify:track:' + track['id']
-    trackname = track['name']
-    artists = ""
-    for artist in track['artists']:
-        artists = artists + artist['name'] + " "
-    trackfeatures[trackid] = trackname + ": " + artists
-
-features = sp.audio_features(tracks)
-
-for feature in features:
-    feature['name'] = trackfeatures.get('spotify:track:' + feature['id'])
 
 
-with open('validation_raw_data' + '.csv', 'w', newline='', encoding='utf-8') as f:
-    dict_writer = csv.DictWriter(f, features[0].keys())
+
+def write_playlist(pl_id, user='spotify'):
+    results = sp.user_playlist(user, playlist_id=pl_id)
+
+    tracks = {}
+
+    for track in results['tracks']['items']:
+        trackid = 'spotify:track:' + track['track']['id']
+        trackname = track['track']['name']
+        artists = ""
+        for artist in track['track']['artists']:
+            artists = artists + artist['name'] + " "
+        tracks[trackid] = trackname + ": " + artists
+
+
+
+    features = sp.audio_features(tracks)
+
+    for feature in features:
+        feature['name'] = tracks.get('spotify:track:' + feature['id'])
+        feature['playlist'] = results['name']
+
+    return features
+
+
+results = []
+
+# Spotify playlists
+for pid in ['5tA2x3J6yAaJpa7mHGvhmB', '7g3zHOim2XylTlM7Oittr8',
+            '06KmJWiQhL0XiV6QQAHsmw', '7sVr7SW0RNqNtOsG48pnOh',
+            '3jtuOxsrTRAWvPPLvlW1VR',
+            '37i9dQZF1DXcN1fAVSf7CR', '3qu74M0PqlkSV76f98aqTd',
+            '37i9dQZF1DX4ykqMZqUn3L', '37i9dQZF1DWYEA2z2jQnzS',
+            '37i9dQZF1DWY8scm4hE7Kd', '5ByMwECRoxdBvGtUaRfsgX',
+            '37i9dQZF1DWTyjRnMgESue', '37i9dQZF1DX0XUsuxWHRQd',
+            '37i9dQZF1DXbhVuSJBP0MW', '37i9dQZF1DX76t638V6CA8',
+            '37i9dQZF1DWXMg4uP5o3dm', '76h0bH2KJhiBuLZqfvPp3K',
+            '37i9dQZF1DX1Jg9KZ76Rr9', '37i9dQZF1DWTRqg6ucMOrz',
+            '37i9dQZF1DXbrSeoLHavML', '37i9dQZF1DX6VDO8a6cQME',
+            '37i9dQZF1DWUzFXarNiofw', '37i9dQZF1DWVstK6FYh8Nw',
+            '37i9dQZF1DXcDoDDetPsEg']:
+    results.append(write_playlist(pid))
+
+
+results.append(write_playlist('1YO0nYpOIDlSH6iCTUVoY6', 'kiamusicbox'))
+
+results.append(write_playlist('7KEhJFrCsr8lkQU603nNwH', 'agentedv'))
+
+results.append(write_playlist('69vewjElepFLh1Qz0B84sa', 'indiefolkradio'))
+
+results.append(write_playlist('5E6bf0JPlGSM3cDxFEhiFb', 'billboard.com'))
+
+
+with open('raw_data' + '.csv', 'w', newline='', encoding='utf-8') as f:
+    dict_writer = csv.DictWriter(f, results[0][0].keys())
     dict_writer.writeheader()
-    dict_writer.writerows(features)
+    for result in results:
+        dict_writer.writerows(result)
+
